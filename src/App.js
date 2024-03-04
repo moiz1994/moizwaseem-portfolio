018438eb189b2510c6c2c9cca5ba0af819130e20
moiz1994 <moizwaseem1994@gmail.com>
2024-03-04 16:54:01 +0500

    Added Sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="App ">
+      <div class="sidebar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+      </div>
+
+      <div class="content">
+        <h2>Main Content</h2>
+        <p>This is where your main content goes.</p>
+        
+      </div>      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import './css/sidebar.css'
+import './js/sidebar.js'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
